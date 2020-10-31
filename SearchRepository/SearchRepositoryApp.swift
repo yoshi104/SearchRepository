601af15d68c12a089164c2cb229d9a41da05da8e
yoshi104 <yoshiyukikt@gmail.com>
2020-10-31 11:00:45 +0900

    first commit

---
+++ SearchRepository/SearchRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchRepositoryApp.swift
+//  SearchRepository
+//
+//  Created by Yoshiyuki Kato on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SearchRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
